4fa638c01746cf3434d5903c67b8382088e40dfc
WWK563388548 <wc563388548@gmail.com>
2018-05-23 23:25:20 +0900

    Created layout of register

---
+++ app/src/main/java/com/myself/wwk/mychat/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.myself.wwk.mychat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    // Sign up button
+    public void signUp(View v){
+
+    }
+}
