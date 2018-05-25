a7bc1f58b34b5896e19a38ef7e8489e5ccf77994
WWK563388548 <wc563388548@gmail.com>
2018-05-25 13:27:02 +0900

    add function of register by firebase

--- app/src/main/java/com/myself/wwk/mychat/RegisterActivity.kt
+++ app/src/main/java/com/myself/wwk/mychat/RegisterActivity.kt
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -23,6 +26,7 @@ class RegisterActivity : AppCompatActivity() {
 
     // Sign up button
     fun signUp(v: View) {
+
         var email = register_email.text.toString().trim()
         var userName  = register_username.text.toString().trim()
         var password = register_password.text.toString().trim()
@@ -38,6 +42,51 @@ class RegisterActivity : AppCompatActivity() {
 
     fun registerNewAccount(email: String, password: String, userName: String){
 
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { 
+                    task: Task<AuthResult> ->
+                        if (task.isSuccessful){
+
+                            var currentUser = mAuth!!.currentUser
+                            var userId = currentUser!!.uid
+
+                            /**
+                             * 在database中创建Users表（table）
+                             *
+                             * e.g.: - Users(table)
+                             *          - user_name: "xxxx"
+                             *          - status: "...."
+                             *          - image: image url
+                             *          ....
+                             */
+                            mDataBase = FirebaseDatabase.getInstance().reference
+                                    .child("Users").child(userId)
+
+
+                            // 使用hashmap去构筑user对象
+                            var userObject = HashMap<String, String>()
+                            userObject.put("user_name", userName)
+                            userObject.put("status", "你好")
+                            userObject.put("image", "default")
+                            userObject.put("thumb_image", "default")
+
+                            // 给数据库赋值
+                            mDataBase!!.setValue(userObject).addOnCompleteListener {
+                                task: Task<Void> ->
+                                if (task.isSuccessful){
+                                    Toast.makeText(this, "注册成功!", Toast.LENGTH_LONG)
+                                            .show()
+                                } else {
+                                    Toast.makeText(this, "注册失败..", Toast.LENGTH_LONG)
+                                            .show()
+                                }
+                            }
+
+                        } else {
+                            Toast.makeText(this, "无法注册!!!", Toast.LENGTH_LONG)
+                                    .show()
+                        }
+                }
     }
 
 }
