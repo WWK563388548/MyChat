0aa1db0be50614522bf432c0cefcfdb6a69e4346
WWK563388548 <wc563388548@gmail.com>
2018-05-28 22:50:49 +0900

    add menu of application

--- app/src/main/java/com/myself/wwk/mychat/activities/DashBoardActivity.kt
+++ app/src/main/java/com/myself/wwk/mychat/activities/DashBoardActivity.kt
@@ -1,9 +1,13 @@
 package com.myself.wwk.mychat.activities
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.myself.wwk.mychat.R
 import com.myself.wwk.mychat.adapters.SectionPagerAdapter
 import kotlinx.android.synthetic.main.activity_dash_board.*
@@ -29,5 +33,35 @@ class DashBoardActivity : AppCompatActivity() {
             Toast.makeText(this, username.toString(), Toast.LENGTH_LONG)
                     .show()
         }
+    }
+
+    // 设置menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        super.onOptionsItemSelected(item)
+
+        if (item != null) {
+            if (item.itemId == R.id.logout_id) {
+                // 退出登陆
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+
+            if (item.itemId == R.id.settings_id) {
+                // 进入settingsActivity
+                startActivity(Intent(this, SettingsActivity::class.java))
+
+            }
+        }
+
+        return true
     }
 }
---
+++ app/src/main/java/com/myself/wwk/mychat/activities/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.myself.wwk.mychat.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.myself.wwk.mychat.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
