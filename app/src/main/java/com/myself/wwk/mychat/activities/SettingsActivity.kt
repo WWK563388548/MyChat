2ef3321d940f5bbc763367f1254046622ce05d42
WWK563388548 <wc563388548@gmail.com>
2018-05-30 11:58:51 +0900

    setup settingsActivity

--- app/src/main/java/com/myself/wwk/mychat/activities/SettingsActivity.kt
+++ app/src/main/java/com/myself/wwk/mychat/activities/SettingsActivity.kt
@@ -2,12 +2,46 @@ package com.myself.wwk.mychat.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
 import com.myself.wwk.mychat.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+    var mDatabase: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? = null
+    var mStorageRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        mCurrentUser = FirebaseAuth.getInstance().currentUser
+        var userId = mCurrentUser!!.uid
+        mDatabase = FirebaseDatabase.getInstance().reference
+                .child("Users")
+                .child(userId)
+
+        mDatabase!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot?) {
+                var userName = dataSnapshot!!.child("user_name").value
+                var image = dataSnapshot!!.child("image").value
+                var status = dataSnapshot!!.child("status").value
+                var thumbImage = dataSnapshot!!.child("thumb_image").value
+
+                // 获取用户名和状态
+                settings_username.text = userName.toString()
+                settings_status_tag.text = status.toString()
+
+
+            }
+
+            override fun onCancelled(dataBaseErrorSnapshot: DatabaseError?) {
+
+            }
+        })
     }
 }
